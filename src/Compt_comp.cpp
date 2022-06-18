b6abada20dfb64b3eb7192055f2f371281e5ed54
Vinicius112 <vinicius.medeiros.083@ufrn.edu.br>
2022-06-18 18:35:37 +0000

    complete

--- src/Compt_comp.cpp
+++ src/Compt_comp.cpp
@@ -17,6 +17,37 @@ using namespace std;
 
 
 Compt_comp:: Compt_comp(string leitura, Dados object){
- cout << object.m_arquivo_palavras_c;
+ int contador = 0;
+ vector <string> palavras_c;
+
+  for(int i = 0; i < object.m_palavras_c.size(); i++){
+        palavras_c.push_back(object.m_palavras_c[i].second);
+  }
+
   
+ for(int i = 0; i < object.m_palavras_c.size(); i++){
+   for(int j = 0; j < leitura.size(); j++){
+     if(leitura[j] == palavras_c[i][j]){
+        contador ++;
+
+     }
+     else{
+       break;
+     }
+     }
+   if(contador == leitura.size()){
+        m_vetor_complete.push_back(palavras_c[i]);
+        
+          
+       }
+     contador  = 0;
+ 
+ }
+
+  for(int i = 0; i < m_vetor_complete.size(); i++){
+    cout << m_vetor_complete[i] << endl;
+  }
+
+
+
 }
